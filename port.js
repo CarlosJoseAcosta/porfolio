df76f8a0f899b8a191138dd391b65b1550027f12
CarlosJoseAcosta <116382349+CarlosJoseAcosta@users.noreply.github.com>
2024-11-21 16:00:17 +0000

    comienzo de java

--- port.js
+++ port.js
@@ -1,1 +1,16 @@
+/*bg-gradient-to-b from-slate-500 to-slate-900 h-max**/
+let nav = document.createElement("nav");
+let columnaNav = document.createElement("div");
+let enlacesNav = document.createElement("a");
+let textoNav = document.createElement("p");
+textoNav.textContent = "Introduccion";
+document.body.appendChild(nav);
+nav.appendChild(columnaNav);
+columnaNav.appendChild(enlacesNav);
+enlacesNav.appendChild(textoNav);
+// let div = document.createElement("div");
+// let p = document.createElement("p");
+// p.textContent = "Prueba xd";
 
+// div.append(p)
+// document.body.appendChild(div)
